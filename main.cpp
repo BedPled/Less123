2b46e3d55183b4e5d4639bf05c6879f8e3817849
Alex <alexander.vischnyakov@gmail.com>
2020-10-03 01:26:32 +1000

    и шоколадку...

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
     using namespace std;
     int main() {
-    cout << "Привет Рома" << endl;
-    cout << "Как дела?" << endl;
-    cout << "Прости, что скинул поздно ночью" << endl;
+        cout << "Привет Рома" << endl;
+        cout << "Как дела?" << endl;
+        cout << "Прости, что скинул поздно ночью" << endl;
+        cout << "Я разобрался что и как тут работает спустя долгое время" << endl;
     return 0;
 }
