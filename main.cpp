47b41f0e5f0866d389a82d4a0c0106685de07015
Alex <alexander.vischnyakov@gmail.com>
2020-10-03 01:48:30 +1000

    ааааааа

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <string>
     using namespace std;
     int main() {
         cout << "Привет Рома" << endl;
         cout << "Как дела?" << endl;
+        string buff;
+        cin >> buff;
+        cout << "У меня тоже";
         cout << "Прости, что скинул поздно ночью" << endl;
         cout << "Я разобрался что и как тут работает спустя долгое время" << endl;
         cout << "Вы классный" << endl;
         for(int i = 0; i < 10; i++) cout << "!";
         cout << endl;
+        
         return 0;
 }
